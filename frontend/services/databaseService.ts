9350eb54097d1f6b3a6f56f4e5f444013babd3d5
Hari-RhayzeStudio <hari@rhayzestudio.com>
2025-11-18 23:52:01 +0530

    feat: update project configuration and hosting settings for Firebase deployment

--- frontend/services/databaseService.ts
+++ frontend/services/databaseService.ts
@@ -8,7 +8,8 @@
 
 // The backend server URL.
 // const API_BASE_URL = 'http://localhost:3001';
-const API_BASE_URL='https://pixshop-backend-hqg9.onrender.com/';
+// const API_BASE_URL='https://pixshop-backend-hqg9.onrender.com';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 /**
  * Updates a product in the database by sending data to the backend API.
